c163da790bbe7e7c6a44dc6e537f6c06eda79b15
dylan wang <zywang_dylan@126.com>
2018-08-10 14:44:29 +0800

    add page date

--- pages/add/add.js
+++ pages/add/add.js
@@ -140,7 +140,8 @@ Page({
       "longitude": longitude
     };
 
-    console.log("id",id)
+    console.log("event",event)
+    
     let self = this;
     wx.request({
       url: app.globalData.apiHost + `/events`,
